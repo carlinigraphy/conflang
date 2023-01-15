9c44b61eadf2e802a4fc91e793241656a1aa0d43
Marcus Aurelius <github-hre-utils@a.aurelius.org>
2023-01-15 11:10:28 -0800

    Clean up api, remove notes

--- src/api.sh
+++ src/api.sh
@@ -13,14 +13,9 @@ function conf {
    for arg in "$@" ; do
       local -n d=$RV
 
-      # TODO: profiling
-      # If you're suuuuper sure you're not going to have any invalid selectors,
-      # taking this out shaves like 20% off the query time.
-      #
-      # Test if the selector exists. If it's trying to query an index that's
-      # *UNSET*, rather than just declared as an empty string, it explodes.
       if [[ ! "${d[$arg]+_}" ]] ; then
-         raise index_error "$arg"
+         printf 'Name Error: [%s] not found\n' "$arg"
+         exit 23
       fi
 
       RV="${d[$arg]}"
